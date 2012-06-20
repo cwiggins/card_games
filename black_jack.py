a2d8b78fd76bbe98370d310dede1d1e8882a1411
Curtiss J Wiggins <cxly15@gmail.com>
2012-06-20 16:09:56 -0400

    all black jack files are now in their own module

--- black_jack.py
+++ black_jack.py
@@ -85,27 +85,3 @@ def determine_face(card):
 	else:
 		return face + 1;
 
-deck = create_deck()
-print deck
-index = 'y'
-while index != 'n':
-	card_1 = deal_hand(deck)
-	card_2 = deal_hand(deck)
-	print card_1, card_2
-	card_1_score = hand_score(card_1)
-	card_2_score = hand_score(card_2)
-	card_1_suit = determine_suit(card_1)
-	card_2_suit = determine_suit(card_2)
-	card_1_face = determine_face(card_1)
-	card_2_face = determine_face(card_2)
-	print card_1_face, card_2_face
-	print "score is:",card_1_score + card_2_score
-	print "With a:", card_1_face, "of", card_1_suit
-	print "And", card_2_face, "of", card_2_suit
-
-	print "do you want to play again"
-	choice = raw_input("> ")
-	if choice == 'y' or choice == "yes":
-		index = 'y';
-	else:
-		index = 'n';
